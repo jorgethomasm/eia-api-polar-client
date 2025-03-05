ed9211dcc2ee9a072199beed06e9061ad7d750e2
Jorge Thomas <jorgethomasm@hotmail.com>
2025-03-05 12:26:50 +0100

    Developing get_electricity_data method

--- eia_client/eia_client.py
+++ eia_client/eia_client.py
@@ -1,3 +1,4 @@
+import datetime
 import requests
 import polars as pl
 
@@ -15,9 +16,70 @@ class EIAClient:
         response.raise_for_status()
         return response.json()
 
-    def get_electricity_data(self, series_id):
-        endpoint = f"electricity/series/id/{series_id}"
-        data = self.get_data(endpoint)
+    def get_electricity_data(self, api_key, api_path, data="value", facets=None, start=None, end=None, length=None,
+                             offset=None, frequency=None):
+        """"
+        Dedicated method for route: electricity
+        and hourly demand by subregion: rto
+        """
+
+        if facets is None:
+            fc = ""
+        else:
+            fc = ""
+        for i in facets.keys():
+            if type(facets[i]) is list:
+                for n in facets[i]:
+                    fc = fc + "&facets[" + i + "][]=" + n
+            elif type(facets[i]) is str:
+                fc = fc + "&facets[" + i + "][]=" + facets[i]
+
+        if start is None:
+            s = ""
+        else:
+            if type(start) is datetime.date:
+                s = "&start=" + start.strftime("%Y-%m-%d")
+            elif type(start) is datetime.datetime:
+                s = "&start=" + start.strftime("%Y-%m-%dT%H")
+            else:
+                print("Error: The start argument is not a valid date or time object")
+                return
+
+        if end is None:
+            e = ""
+        else:
+            if type(end) is datetime.date:
+                e = "&end=" + end.strftime("%Y-%m-%d")
+            elif type(end) is datetime.datetime:
+                e = "&end=" + end.strftime("%Y-%m-%dT%H")
+            else:
+                print("Error: The end argument is not a valid date or time object")
+                return
+
+        if length is None:
+            l = ""
+        else:
+            l = "&length=" + str(length)
+
+        if offset is None:
+            o = ""
+        else:
+            o = "&offset=" + str(offset)
+
+        if frequency is None:
+            fr = ""
+        else:
+            fr = "&frequency=" + str(frequency)
+
+        url = "https://api.eia.gov/v2/" + api_path + "?data[]=value" + fc + s + e + l + o + fr
+
+
+        endpoint = f"electricity/rto/id/{series_id}"
+
+        # Call get_data
+        data = self.get_data(endpoint)  # as json
+
         # Convert JSON to Polars DataFrame
         df = pl.DataFrame(data["response"]["data"])
+
         return df
--- examples/main.py
+++ examples/main.py
@@ -1,11 +1,36 @@
 import os
+import datetime
 from eia_client import EIAClient
 
 
 # Example usage
 if __name__ == "__main__":
+
     api_key = os.getenv("EIA_API_KEY")
     client = EIAClient(api_key)
+
+    freq = "hourly"
+    facets = {
+        "parent": "CISO",
+        "subba": "SDGE"
+    }
+
+    """
+    https://api.eia.gov/v2/electricity/rto/region-sub-ba-data/data/?frequency=hourly&data[0]=value&facets[parent][]=CISO&facets[subba][]=SDGE&start=2025-01-01T00&end=2025-02-28T00&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000
+    
+    
+    https://www.eia.gov/opendata/browser/electricity/rto/region-sub-ba-data?frequency=hourly&data=value;&facets=parent;subba;&parent=CISO;&subba=SDGE;&start=2025-01-01T00&end=2025-02-28T00&sortColumn=period;&sortDirection=desc;
+    """
+
+    dt_start = datetime.datetime(2024, 1, 1, 1)
+    dt_end = datetime.datetime(2024, 1, 31, 23)
     series_id = "ELECTRICITY_SERIES_ID_HERE"
-    df = client.get_electricity_data(series_id)
+
+    df = client.get_electricity_data(api_key=api_key,
+                                     api_path=api_path,
+                                     frequency=freq,
+                                     facets=facets,
+                                     start=dt_start,
+                                     end=dt_end)
+
     print(df)
