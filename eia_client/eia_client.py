ab97d6d77ac520654eb4e4d28f76cee188350c1a
Jorge Thomas <jorgethomasm@hotmail.com>
2025-03-08 14:26:53 +0100

    Improved performance of offset methods, now helper functions

--- eia_client/eia_client.py
+++ eia_client/eia_client.py
@@ -4,30 +4,52 @@ from typing import Optional, Union
 import polars as pl
 
 
+# ================ Helper functions ================
+def day_offset(start: datetime.date, end: datetime.date, offset: int) -> list:
+    """
+    Generate a list of date type elements with the given offset
+    representing days. This helper function is for the back-fill operation,
+    given that the maximum request size of the API is of approximate 2500 data points.
+    """
+    current = [start]
+    last_date = start
+    while last_date < end:
+        next_date = last_date + datetime.timedelta(days=offset)
+        if next_date < end:
+            current.append(next_date)
+            last_date = next_date
+        else:
+            break
+    current.append(end)
+    return current
+
+
+def hour_offset(start: datetime.datetime, end: datetime.datetime, offset: int) -> list:
+    """
+    Generate a list of datetime type elements with the given offset
+    representing hours. This helper function is for the back-fill operation,
+    given that the maximum request size of the API is of approximate 2500 data points.
+    """
+    current = [start]
+    last_datetime = start
+    while last_datetime < end:
+        next_datetime = last_datetime + datetime.timedelta(hours=offset)
+        if next_datetime < end:
+            current.append(next_datetime)
+            last_datetime = next_datetime
+        else:
+            break
+    current.append(end)
+    return current
+
+
+# ==================================================
+
 class EIAClient:
     BASE_URL = "https://api.eia.gov/v2/"
 
     def __init__(self, api_key):
         self.api_key = api_key
-
-    # Helper (private) methods:
-    def __day_offset(start, end, offset) -> list:
-        current = [start]
-        while max(current) < end:
-            if max(current) + datetime.timedelta(days=offset) < end:
-                current.append(max(current) + datetime.timedelta(days=offset))
-        else:
-            current.append(end)
-        return current
-
-    def __hour_offset(start, end, offset) -> list:
-        current = [start]
-        while max(current) < end:
-            if max(current) + datetime.timedelta(hours=offset) < end:
-                current.append(max(current) + datetime.timedelta(hours=offset))
-            else:
-                current.append(end)
-        return current
 
     def __get_data(self, endpoint: str, params=None):
         params = params or {}
@@ -38,8 +60,8 @@ class EIAClient:
         response.raise_for_status()
         return response.json()
 
-    def get_electricity_data(self, 
-                             api_path: str, 
+    def get_electricity_data(self,
+                             api_path: str,
                              facets: Optional[dict] = None,
                              start: Optional[Union[datetime.date, datetime.datetime]] = None,
                              end: Optional[Union[datetime.date, datetime.datetime]] = None,
@@ -50,20 +72,20 @@ class EIAClient:
         route: electricity
         rto: real-time grid monitor
         """
-        
+
         # Check if facets is not a string, list, or None
         if facets is not None and not isinstance(facets, dict):
             raise TypeError("facets must be a dictionary or None")
 
         # Create string var for facet or extract info from the list
-        if facets is None:
-            facet_str = ""
-        else:
-            facet_str = ""
+        facet_str = ""
+
+        if facets is not None:
             for i in facets.keys():
                 if type(facets[i]) is list:
-                    for n in facets[i]:
-                        facet_str = facet_str + "&facets[" + i + "][]=" + n
+                    for facet in facets[i]:
+                        # Un-list and concatenate facets
+                        facet_str = facet_str + "&facets[" + i + "][]=" + facet
                 elif type(facets[i]) is str:
                     facet_str = facet_str + "&facets[" + i + "][]=" + facets[i]
 
@@ -75,7 +97,7 @@ class EIAClient:
         elif type(start) is datetime.date:
             start = "&start=" + start.strftime("%Y-%m-%d")
         else:
-            start = "&start=" + start.strftime("%Y-%m-%dT%H")           
+            start = "&start=" + start.strftime("%Y-%m-%dT%H")
 
         if end is not None and not isinstance(end, (datetime.date, datetime.datetime)):
             raise TypeError("end must be a date, datetime, or None")
@@ -104,7 +126,7 @@ class EIAClient:
 
         endpoint = "electricity/" + api_path + "?data[]=value" + facet_str + start + end + length + offset + frequency
 
-        # Call get_data
+        # Call private method __get_data
         data = self.__get_data(endpoint)  # as json
 
         # Convert JSON to Polars DataFrame
@@ -121,69 +143,45 @@ class EIAClient:
 
         return df
 
-    # def eia_backfill(start, end, offset, api_key, api_path, facets):
-    #         class response:
-    #             def __init__(output, data, parameters):
-    #                 output.data = data
-    #                 output.parameters = parameters
-    #
-    #         if type(api_key) is not str:
-    #             print("Error: The api_key argument is not a valid string")
-    #             return
-    #         elif len(api_key) != 40:
-    #             print("Error: The length of the api_key is not valid, must be 40 characters")
-    #             return
-    #
-    #         if api_path[-1] != "/":
-    #             api_path = api_path + "/"
-    #
-    #         if type(start) is datetime.date:
-    #             s = "&start=" + start.strftime("%Y-%m-%d")
-    #         elif type(start) is datetime.datetime:
-    #             s = "&start=" + start.strftime("%Y-%m-%dT%H")
-    #         else:
-    #             print("Error: The start argument is not a valid date or time object")
-    #             return
-    #
-    #         if type(end) is datetime.date:
-    #             e = "&end=" + end.strftime("%Y-%m-%d")
-    #         elif type(end) is datetime.datetime:
-    #             e = "&end=" + end.strftime("%Y-%m-%dT%H")
-    #         else:
-    #             print("Error: The end argument is not a valid date or time object")
-    #             return
-    #
-    #         if type(start) is datetime.date:
-    #             time_vec_seq = day_offset(start=start, end=end, offset=offset)
-    #         elif type(start) is datetime.datetime:
-    #             time_vec_seq = hour_offset(start=start, end=end, offset=offset)
-    #
-    #         for i in range(len(time_vec_seq[:-1])):
-    #             start = time_vec_seq[i]
-    #             if i < len(time_vec_seq[:-1]) - 1:
-    #                 end = time_vec_seq[i + 1] - datetime.timedelta(hours=1)
-    #             elif i == len(time_vec_seq[:-1]) - 1:
-    #                 end = time_vec_seq[i + 1]
-    #             temp = eia_get(api_key=api_key,
-    #                            api_path=api_path,
-    #                            facets=facets,
-    #                            start=start,
-    #                            data="value",
-    #                            end=end)
-    #             if i == 0:
-    #                 df = temp.data
-    #             else:
-    #                 df = df._append(temp.data)
-    #
-    #         parameters = {
-    #             "api_path": api_path,
-    #             "data": "value",
-    #             "facets": facets,
-    #             "start": start,
-    #             "end": end,
-    #             "length": None,
-    #             "offset": offset,
-    #             "frequency": None
-    #         }
-    #         output = response(data=df, parameters=parameters)
-    #         return output
+    def eia_back_fill(self,
+                     start,
+                     end,
+                     offset,
+                     api_key,
+                     api_path,
+                     facets):
+
+        if type(start) is datetime.date:
+            time_vec_seq = self.__day_offset(start=start, end=end, offset=offset)
+        elif type(start) is datetime.datetime:
+            time_vec_seq = self.__hour_offset(start=start, end=end, offset=offset)
+
+        for i in range(len(time_vec_seq[:-1])):
+            start = time_vec_seq[i]
+            if i < len(time_vec_seq[:-1]) - 1:
+                end = time_vec_seq[i + 1] - datetime.timedelta(hours=1)
+            elif i == len(time_vec_seq[:-1]) - 1:
+                end = time_vec_seq[i + 1]
+            temp = eia_get(api_key=api_key,
+                           api_path=api_path,
+                           facets=facets,
+                           start=start,
+                           data="value",
+                           end=end)
+            if i == 0:
+                df = temp.data
+            else:
+                df = df.append(temp.data)
+
+        parameters = {
+            "api_path": api_path,
+            "data": "value",
+            "facets": facets,
+            "start": start,
+            "end": end,
+            "length": None,
+            "offset": offset,
+            "frequency": None
+        }
+        output = response(data=df, parameters=parameters)
+        return output
--- examples/main.py
+++ examples/main.py
@@ -27,10 +27,47 @@ if __name__ == "__main__":
     dt_start = datetime.datetime(2024, 1, 1, 1)
     dt_end = datetime.datetime(2024, 1, 25, 23)
 
-    df = client.get_electricity_data(api_path=api_path,
-                                     frequency=freq,
-                                     facets=facets,
-                                     start=dt_start,
-                                     end=dt_end)
+    def day_offset(start: datetime.date, end: datetime.date, offset: int) -> list:
+        """
+        Generate a list of date type elements with the given offset
+        representing days. This function is for the back-fill operation,
+        given that the maximum request size of the API is of approximate 2500 data points.
+        """
+        current = [start]
+        while max(current) < end:
+            if max(current) + datetime.timedelta(days=offset) < end:
+                current.append(max(current) + datetime.timedelta(days=offset))
+            else:
+                current.append(end)
+        return current
 
-    print(df)
+    d_start = datetime.date(2024, 1, 1)
+    d_end = datetime.date(2024, 1, 25)
+
+
+    def day_offset2(start: datetime.date, end: datetime.date, offset: int) -> list:
+        """
+        Generate a list of date type elements with the given offset
+        representing days. This function is for the back-fill operation,
+        given that the maximum request size of the API is of approximate 2500 data points.
+        """
+        current = [start]
+        last_date = start
+
+        while last_date < end:
+            next_date = last_date + datetime.timedelta(days=offset)
+            if next_date < end:
+                current.append(next_date)
+                last_date = next_date
+            else:
+                break
+        current.append(end)
+
+        return current
+
+    test_vec = day_offset2(d_start,d_end, 10)
+
+    print(test_vec)
+    #df = client.get_electricity_data(api_path=api_path, frequency=freq, facets=facets, start=dt_start, end=dt_end)
+
+    #print(df)
